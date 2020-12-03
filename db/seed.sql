7295c124a13553895b768950d2609365adf36161
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-03 05:30:39 -0600

    added seeds

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Bacon Burger', false);
